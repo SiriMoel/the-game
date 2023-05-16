c666210364fcc5cc47923360e4a29009d0b679f2
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2023-05-16 15:40:59 +1000

    i push

--- data/player/Inventory.cs
+++ data/player/Inventory.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Inventory : Node2D
+public partial class Inventory : Control
 {
 	public Items.Item[] InvItems = new Items.Item[20];
 	public void AddItem(string itemId, int? slot) {
@@ -40,4 +40,62 @@ public partial class Inventory : Node2D
 		GetParent().AddChild(i);
 	}
 
+	public bool InventoryShown = false;
+	public int ActiveSlot = 1;
+
+	public override void _Ready()
+	{
+		Visible = false;
+		
+	}
+
+	public override void _Process(double delta)
+	{
+		TextureRect SelectedSlot = GetNode<TextureRect>("SelectedSlot");
+
+		if (Input.IsActionJustPressed("inventory")) {	
+			InventoryShown = !InventoryShown;
+			Visible = !Visible;
+			GetTree().Paused = !GetTree().Paused;
+		}
+
+		if (InventoryShown == true) {
+			if (Input.IsActionJustPressed("up")) {
+				ActiveSlot -= 5;
+			}
+			if (Input.IsActionJustPressed("down")) {
+				ActiveSlot += 5;
+			}
+			if (Input.IsActionJustPressed("left")) {
+				ActiveSlot -= 1;
+			}
+			if (Input.IsActionJustPressed("right")) {
+				ActiveSlot += 1;
+			}
+			if (ActiveSlot < 1) {
+				ActiveSlot = 1;
+			}
+			if (ActiveSlot > 20) {
+				ActiveSlot = 20;
+			}
+
+			SelectedSlot.Position = GetNode<TextureRect>("Slot{ActiveSlot}").Position;
+		}
+	
+	}
+
 }
+
+/*
+
+X X X X X
+X X X X X
+X X X X X
+X X X X X
+
+01 02 03 04 05
+06 07 08 09 10
+11 12 13 14 15
+16 17 18 19 20
+
+*/
